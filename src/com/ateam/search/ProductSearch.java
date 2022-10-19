2a93b517801c24d4a00e2bf1b3bc8011e328f3b2
Lenovoyuka <oyamayu0@gmail.com>
2022-10-19 14:21:18 +0900

    微

--- src/com/ateam/search/ProductSearch.java
+++ src/com/ateam/search/ProductSearch.java
@@ -1,8 +1,17 @@
 package com.ateam.search;
 
+import java.util.Scanner;
+
 public class ProductSearch {
 	public void execute() {
 		System.out.println("★商品検索を実行します");
+		System.out.println("検索キーワードを入力して下さい");
+		Scanner mode = new Scanner(System.in);
+		String modeStr = mode.next();
+		System.out.println("キーワード>>>");
+
+		
+	
 	}
 
 }
