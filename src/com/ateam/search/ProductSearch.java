10d7c5d6fea40751247245af9743c6c4480a4e78
Lenovoyuka <oyamayu0@gmail.com>
2022-10-24 15:21:36 +0900

    add up

--- src/com/ateam/search/ProductSearch.java
+++ src/com/ateam/search/ProductSearch.java
@@ -10,44 +10,39 @@ public class ProductSearch {
 
 	public void execute() {
 		while (true) {
-		System.out.println("----------------------------------");
-		System.out.println("★商品検索を実行します");
-		System.out.println("検索キーワードを入力して下さい");
-		System.out.println("----------------------------------");
-		System.out.println("Keyword>>>");
-		Scanner mode = new Scanner(System.in);
-		String modeStr = mode.next();
-		//キーボードから商品IDの入力を受け付ける
-		//String searchTargetId = new java.util.Scanner(System.in).next();
+			System.out.println("----------------------------------");
+			System.out.println("★商品検索を実行します");
+			System.out.println("検索キーワードを入力して下さい");
+			System.out.println("----------------------------------");
+			System.out.println("Keyword>>>");
+			Scanner mode = new Scanner(System.in);
+			String modeStr = mode.next();
+			//キーボードから商品IDの入力を受け付ける
+			//String searchTargetId = new java.util.Scanner(System.in).next();
 
-		//csvファイルの読み込み
-		BufferedReader br = null;
-		String str = "";
-		String[] productInfo = null;
-		try {
-			br = new BufferedReader(
-					new FileReader("C:\\pleiades2022\\workspace\\TwiceProductManagent\\Sampledata.csv"));
-			while ((str = br.readLine()) != null) {
-				productInfo = str.split(",");
-				//★商品データを表示				
-				System.out.println(java.util.Arrays.toString(productInfo));
-				//				System.out.println(productInfo);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
+			//csvファイルの読み込み
+			BufferedReader br = null;
+			String str = "";
+			String[] productInfo = null;
 			try {
-				br.close();
+				br = new BufferedReader(
+						new FileReader("C:\\pleiades2022\\workspace\\TwiceProductManagent\\Sampledata.csv"));
+				while ((str = br.readLine()) != null) {
+					productInfo = str.split(",");
+					//★商品データを表示				
+					System.out.println(java.util.Arrays.toString(productInfo));
+					//				System.out.println(productInfo);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					br.close();
 
-			} catch (IOException e) {
-			}
-		}
-
-			
-
-
+				} catch (IOException e) {
+				}
 				System.out.println("----------------------------------");
-				
+
 				String ｓ = new java.util.Scanner(System.in).nextLine();
 				if (ｓ.equals("Y")) {
 					System.out.println();
@@ -63,11 +58,10 @@ public class ProductSearch {
 					//１の場合delメソッドのはじめに戻る処理
 					//		del();
 				}
-				{
-					//２の場合メニュー画面に戻る処理
-
-				}
 			}
-		}
+			return;
 
+		}
 	}
+
+}
