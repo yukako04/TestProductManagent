3d733fdb2f2bc86ec9113767bb3047ccb64d937f
Lenovoyuka <oyamayu0@gmail.com>
2022-10-24 13:12:25 +0900

    Edditing in progress⑥

--- src/com/ateam/search/ProductSearch.java
+++ src/com/ateam/search/ProductSearch.java
@@ -6,13 +6,9 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class ProductSearch {
+	private String[][] productInfoList;
+
 	public void execute() {
-		System.out.println("★商品検索を実行します");
-		System.out.println("検索キーワードを入力して下さい");
-		System.out.println("----------------------------------");
-		System.out.println("Keyword>>>");
-		Scanner mode = new Scanner(System.in);
-		String modeStr = mode.next();
 
 		//csvファイルの読み込み
 		BufferedReader br = null;
@@ -34,8 +30,66 @@ public class ProductSearch {
 				br.close();
 
 			} catch (IOException e) {
-				e.printStackTrace();
 			}
+		}
+		while (true) {
+			System.out.println("----------------------------------");
+			System.out.println("★商品検索を実行します");
+			System.out.println("検索キーワードを入力して下さい");
+			System.out.println("----------------------------------");
+			System.out.println("Keyword>>>");
+			Scanner mode = new Scanner(System.in);
+			String modeStr = mode.next();
+			//キーボードから商品IDの入力を受け付ける
+			//String searchTargetId = new java.util.Scanner(System.in).next();
+
+			
+			String[] searchProductInfo = null;
+			String searchTargetKeyword = null;
+			for (String[] product : productInfoList) {
+				if (product[0].equals(searchTargetKeyword)) {
+					searchProductInfo = product;
+					break;
+				}
+			}
+
+			if (searchProductInfo == null) {
+				System.out.println("該当する商品データがありません");
+			} else {
+
+				System.out.println("商品ID＝" + searchTargetKeyword);
+				System.out.println("商品コード＝");
+				System.out.println("商品名＝");
+				System.out.println("商品分類＝");
+				System.out.println("販売単価＝");
+				System.out.println("仕入単価＝");
+				System.out.println("登録日");
+
+				System.out.println("----------------------------------");
+				
+				System.out.print("商品情報を削除しますか？Y/N＞");
+				String ｓ = new java.util.Scanner(System.in).nextLine();
+				if (ｓ.equals("Y")) {
+					System.out.println("商品情報を削除しました。");
+					System.out.println();
+					System.out.println("続けて商品を検索しますか？");
+				} else {
+					System.out.println("続けて商品を検索しますか？");
+				}
+
+				System.out.print("1:続けて検索する 2:メニューへ戻る > 1");
+				int input2 = new java.util.Scanner(System.in).nextInt();
+
+				if (input2 == 1) {
+					//１の場合delメソッドのはじめに戻る処理
+					//		del();
+				}
+				{
+					//２の場合メニュー画面に戻る処理
+
+				}
+			}
+			return;
 		}
 
 	}
