6cd3b0cbd3bc51089b96a2415ee117021763ed3f
Lenovoyuka <oyamayu0@gmail.com>
2022-10-26 07:21:36 +0900

    7:20

--- src/com/ateam/search/ProductSearch.java
+++ src/com/ateam/search/ProductSearch.java
@@ -3,10 +3,9 @@ package com.ateam.search;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class ProductSearch {
-	private String[][] productInfoList;
+//	private String[][] productInfoList;
 
 	public void execute() {
 		while (true) {
@@ -15,12 +14,12 @@ public class ProductSearch {
 			System.out.println("検索キーワードを入力して下さい");
 			System.out.println("----------------------------------");
 			System.out.println("Keyword>>>");
-			Scanner mode = new Scanner(System.in);
-			String modeStr = mode.next();
-
+			//Scanner mode = new Scanner(System.in);
+			//String modeStr = mode.next();
 			//キーボードからキーワードの入力を受け付ける
-			//String searchTargetId = new java.util.Scanner(System.in).next();
-
+			@SuppressWarnings("resource")
+			String searchTargetKeyword = new java.util.Scanner(System.in).nextLine();
+			
 			//csvファイルの読み込み
 			BufferedReader br = null;
 			String str = "";
@@ -31,8 +30,19 @@ public class ProductSearch {
 				while ((str = br.readLine()) != null) {
 					productInfo = str.split(",");
 					//★商品データを表示				
-					System.out.println(java.util.Arrays.toString(productInfo));
-					//				System.out.println(productInfo);
+					//System.out.println(java.util.Arrays.toString(productInfo));
+					//System.out.println(productInfo);
+					if(productInfo.equals(searchTargetKeyword)) {
+						System.out.print(productInfo[0]);
+						System.out.println(productInfo[1]);
+						System.out.println(productInfo[2]);
+					}else {
+						System.out.println(productInfo);
+						break;
+
+					}
+			
+					
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -41,6 +51,8 @@ public class ProductSearch {
 					br.close();
 
 				} catch (IOException e) {
+					e.printStackTrace();
+
 				}
 			}
 			System.out.println("----------------------------------");
@@ -52,8 +64,9 @@ public class ProductSearch {
 			if(inputNextMenu.equals("2")) {
 				break;
 			}
-			}
+			
 
 		}
 	}
+}
 
